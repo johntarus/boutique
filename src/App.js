c696157ddc17df2d8dbfa35986efa74c3125e4f4
tarus <tarusjohn96@gmail.com>
2022-10-09 00:58:08 +0300

    conventional commits

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 function App() {
     return (
         <div className="App">
-            <p className="text-red-600">hello</p>
+            <p className="text-red-600">hello kenyan </p>
         </div>
     )
 }
